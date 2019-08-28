d092b65a296fbe3da642fa0ad315651e2faddb1b
med djelaili <meddjelaili.me@gmail.com>
2019-08-28 16:59:58 +0100

    rename build folder to docs

---
+++ docs/precache-manifest.2867e95b9a26a8da3e318b15ac669d89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0965146f8f86845e4a0dd14304fee8e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3aa177e6594151a66d44",
+    "url": "/static/css/main.a24e969d.chunk.css"
+  },
+  {
+    "revision": "a1acbade52767b1a506b",
+    "url": "/static/js/2.c7817c48.chunk.js"
+  },
+  {
+    "revision": "3aa177e6594151a66d44",
+    "url": "/static/js/main.18ba44b4.chunk.js"
+  },
+  {
+    "revision": "03122bbe4f1391793988",
+    "url": "/static/js/runtime~main.0a32829c.js"
+  },
+  {
+    "revision": "9bebbbb8206cfc0d2a851f75714d5217",
+    "url": "/static/media/envelope.9bebbbb8.svg"
+  },
+  {
+    "revision": "d5aadc40d1c2abce9aa4382def2578e4",
+    "url": "/static/media/questions.d5aadc40.svg"
+  }
+]);
